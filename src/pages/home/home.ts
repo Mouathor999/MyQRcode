e81468096f79d39a638e1f37df57fb908e3df148
Mouathor <Mouthor1995@gmail.com>
2017-09-27 23:09:56 +0700

    my original app

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import {} from 'i'
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
